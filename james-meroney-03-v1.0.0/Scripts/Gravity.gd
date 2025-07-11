490e675e837ec36008497eceff4b19980544c4e6
RAGEJames <137014538+RAGEJames@users.noreply.github.com>
2025-07-10 22:51:22 -0400

    Delete james-meroney-03-v1.0.0 directory

--- james-meroney-03-v1.0.0/Scripts/Gravity.gd
+++
@@ -1,4 +0,0 @@
-extends CharacterBody2D
-func _physics_process(delta: float) -> void:
-	if not get_parent().get_node("Player").is_on_floor():
-		get_parent().get_node("Player").velocity += get_gravity() * delta
--- james-meroney-03-v1.0.0/Scripts/gui.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-func _process(delta: float) -> void:
-	if get_parent().get_node("W1").is_visible():
-		get_node("Base").position = Vector2(get_parent().get_node("W1/PLAYER/Player").position)
-	if get_parent().get_node("W2").is_visible():
-		get_node("Base").position = Vector2(get_parent().get_node("W2/PLAYER/Player").position)
